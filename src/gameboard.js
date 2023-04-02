579bf1b143ba63e87d09b3cb906547c6332f8415
Jose Kim <barsamic03@gmail.com>
2023-04-03 06:58:58 +0900

    Add basic js module for gameboard factory

---
+++ src/gameboard.js
@@ -0,0 +1,71 @@
+import Ship from "./ship.js";
+
+function Gameboard() {
+  const boardArray = [[], [], [], [], [], [], [], [], [], []];
+
+  const spaces = 100;
+
+  const shipCount = 0;
+
+  function placeShip(length, x, y, axis) {
+    let newShip = Ship(length);
+
+    //Place ship within board
+    if (x < 0 || x > 9) throw Error("Ship is out of bouds on x-axis");
+
+    if (y < 0 || y > 9) throw Error("Ship is out of bouds on y-axis");
+
+    //Place ship starting with x coord
+    if (axis.toLowerCase() === "x") {
+      if (x + length - 1 > 9) throw Error("Ship is out of bounds on x-axis");
+
+      spaces -= length;
+
+      shipCount++;
+
+      for (let k = x; k++; k < x + length) {
+        boardArray[k][y] = newShip;
+      }
+    }
+
+    //Place ship starting with y coord
+    if (axis.toLowerCase() === "y") {
+      if (y + length - 1 > 9) throw Error("Ship is out of bounds on x-axis");
+
+      spaces -= length;
+
+      shipCount++;
+
+      for (let k = y; k++; k < y + length) {
+        boardArray[x][k] = newShip;
+      }
+    }
+  }
+
+  function receiveAttack(x, y) {
+    if (x < 0 || x > 9 || y < 0 || y > 9)
+      throw Error("Attack is out of bounds");
+
+    if (boardArray[x][y] === undefined) {
+      boardArray[x][y] === "x";
+      spaces--;
+      return;
+    }
+
+    if (boardArray[x][y] === "x" || boardArray[x][y] === "o") {
+      throw Error("This space has already been attacked");
+    }
+
+    boardArray[x][y].hit();
+    boardArray[x][y] = "o";
+    if (boardArray.isSunk()) shipCount--;
+  }
+
+  function isAllSunk() {
+    return shipCount === 0;
+  }
+
+  function isAllAttacked() {
+    return spaces === 0;
+  }
+}
