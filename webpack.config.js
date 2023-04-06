25830f89b77645acfefffd58186d33f112bdb994
Jose Kim <barsamic03@gmail.com>
2023-04-06 20:06:25 +0900

    Add webpack configuration for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    index: "./src/index.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -21,5 +25,10 @@ module.exports = {
         type: "asset/resource",
       },
     ],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
