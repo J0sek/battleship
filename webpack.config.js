38d167e6e403ac9a1f3f24567d4822389c12f537
Jose Kim <barsamic03@gmail.com>
2023-04-06 20:07:48 +0900

    Remove html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     index: "./src/index.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
+
   module: {
     rules: [
       {
@@ -29,6 +24,5 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
 };
