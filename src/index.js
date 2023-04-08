182e9428f2a40c8e7cf8aabe3ec9bb1f54be6404
Jose Kim <barsamic03@gmail.com>
2023-04-08 14:34:16 +0900

    Move side effects in menu.js to index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,25 @@ import Player from "./player.js";
 import Ship from "./ship.js";
 import "./menu.js";
 import "./style.css";
+
+let player;
+
+let computer = Player("Computer");
+
+let playerBoard = Gameboard();
+
+let computerBoard = Gameboard();
+
+const startForm = document.querySelector(".player-info-form");
+
+startForm.addEventListener("submit", () => {
+  event.preventDefault();
+
+  startForm.style.display = "none";
+
+  let playerNameInfo = document.querySelector("#player-name");
+
+  playerNameInfo = playerNameInfo.value;
+
+  player = Player(playerNameInfo);
+});
--- src/menu.js
+++
@@ -1,7 +0,0 @@
-const startForm = document.querySelector(".player-info-form");
-
-startForm.addEventListener("submit", () => {
-  event.preventDefault();
-
-  startForm.style.display = "none";
-});
